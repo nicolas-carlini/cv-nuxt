20baa8774cce130ddff7c1dc3d6a4c7c6aa0ff51
nicolas_carlini <nicolascarlini1@gmail.com>
2021-06-11 22:27:46 -0300

    change names

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       id="sideNav"
     >
       <a class="navbar-brand js-scroll-trigger" href="#page-top">
-        <span class="d-block d-lg-none">Resume Iván</span>
+        <span class="d-block d-lg-none">Resume Nicolas</span>
         <span class="d-none d-lg-block">
           <img
             class="img-fluid img-profile rounded-circle mx-auto mb-2"
@@ -112,7 +112,7 @@ export default {
   },
   head() {
     return {
-      title: "Iván Greve"
+      title: "N. Carlini"
     };
   },
   methods: {
